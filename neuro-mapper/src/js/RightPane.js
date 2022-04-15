6babab64c5911e05e8ad4e9276ab5d7ca88ea9f4
Haekyu Park <haekyu@ipsec-10-2-194-150.vpn.gatech.edu>
2022-04-14 23:23:09 -0400

    resolved errors

--- neuro-mapper/src/js/RightPane.js
+++ neuro-mapper/src/js/RightPane.js
@@ -1,14 +1,5 @@
-import { useEffect, useRef } from "react";
-import * as constant from "./constant.js";
 import { observer } from "mobx-react";
 
-export const RightPane = observer(
-    ({
-      store
-    }) => { 
-        return (
-        <div id="right-pane-wrapper">
-        </div>
-        )
-    }
-)
+export const RightPane = observer(({ store }) => {
+  return <div id="right-pane-wrapper"></div>;
+});
