36c02188ce091cef5babe81b3e4dc1417f1580fa
Haekyu Park <haekyu@ipsec-10-2-194-150.vpn.gatech.edu>
2022-04-14 23:31:12 -0400

    epoch in epoch slider is updated by epoch updates, not by dom changes

--- neuro-mapper/src/js/EpochControl.js
+++ neuro-mapper/src/js/EpochControl.js
@@ -4,52 +4,45 @@ import { observer } from "mobx-react";
 import { Slider } from "@mui/material";
 import { ScatterGL, RenderMode } from "scatter-gl";
 
-export const EpochControl = observer(
-    ({
-      store
-    }) => { 
-        const handleSliderChange = (e, val) => {
-            let epoch = val
-            store.setEpoch(epoch)
-      
-            for (let i = 0; i < constant.layers.length; i++) {
-              let layer = constant.layers[i]
-              let points = store.embData[layer]
-              let epoch = store.epoch
-              const labels = points.map((point => point['label']))
-              const datapoints = points.map((point => point['emb'][epoch]))
-              const metadata = [];
-              labels.forEach(element => {
-                metadata.push({
-                  labelIndex: element,
-                  label: constant.cifar_10_classes[element]
-                })
-              });
-              const dataset = new ScatterGL.Dataset(datapoints, metadata);
-              dataset.setSpriteMetadata({
-                spriteImage: 'spritesheet.png',
-                singleSpriteSize: [32, 32],
-              });
-              store.plots[i].updateDataset(dataset)
-            }
-            document.getElementById("epoch-val").innerText = `epoch = ${epoch}`
-          }
+export const EpochControl = observer(({ store }) => {
+  const handleSliderChange = (e, val) => {
+    let epoch = val;
+    store.setEpoch(epoch);
 
-        return(
-            <div>
-                <Slider
-                    defaultValue={constant.epochs[0]}
-                    valueLabelDisplay="auto"
-                    step={5}
-                    min={constant.epochs[0]}
-                    max={constant.epochs.slice(-1)[0]}
-                    color="secondary"
-                    onChange={handleSliderChange}
-                />
-                <div id="epoch-val">
-                epoch = {constant.epochs[0]}
-                </div>
-            </div>
-        )
+    for (let i = 0; i < constant.layers.length; i++) {
+      let layer = constant.layers[i];
+      let points = store.embData[layer];
+      let epoch = store.epoch;
+      const labels = points.map((point) => point["label"]);
+      const datapoints = points.map((point) => point["emb"][epoch]);
+      const metadata = [];
+      labels.forEach((element) => {
+        metadata.push({
+          labelIndex: element,
+          label: constant.cifar_10_classes[element],
+        });
+      });
+      const dataset = new ScatterGL.Dataset(datapoints, metadata);
+      dataset.setSpriteMetadata({
+        spriteImage: "spritesheet.png",
+        singleSpriteSize: [32, 32],
+      });
+      store.plots[i].updateDataset(dataset);
     }
-)
+  };
+
+  return (
+    <div id="epoch-control">
+      <Slider
+        defaultValue={constant.epochs[0]}
+        valueLabelDisplay="auto"
+        step={5}
+        min={constant.epochs[0]}
+        max={constant.epochs.slice(-1)[0]}
+        color="secondary"
+        onChange={handleSliderChange}
+      />
+      <div id="epoch-val">epoch = {store.epoch}</div>
+    </div>
+  );
+});
