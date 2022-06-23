c10c76ad0775fa64e2cfd5619aebaee72617ac20
Zhiyan Zhou <zhiyanzhou@Zhiyans-MBP.attlocal.net>
2022-06-22 18:00:26 -0700

    Change Navbar

--- neuro-mapper/src/js/NavBar.js
+++ neuro-mapper/src/js/NavBar.js
@@ -1,6 +1,28 @@
 import { observer } from "mobx-react";
+import Box from '@mui/material/Box';
+import OutlinedInput from '@mui/material/OutlinedInput';
+import MenuItem from '@mui/material/MenuItem';
+import Select from '@mui/material/Select';
+import Chip from '@mui/material/Chip';
+import * as constant from "./constant.js";
+
 
 export const NavBar = observer(({ store }) => {
+  const ITEM_HEIGHT = 48;
+  const ITEM_PADDING_TOP = 8;
+  const MenuProps = {
+    PaperProps: {
+      style: {
+        maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+        width: 250,
+      },
+    },
+  };
+
+  const handleClassNameChange = () => {
+
+  }
+
   return <div className="topnav">
     <div className="logo-text">
       <b>NeuroMapper</b>
